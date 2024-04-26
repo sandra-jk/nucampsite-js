a65072ea8bc00f92e2e98c20618831bc451c78cf
Sandra <sandrajerotich1@gmail.com>
2024-04-26 03:38:27 +0300

    Fetch weather function and weather component

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,25 @@ carouselButton.addEventListener('click', function () {
         carousel.cycle();
     }
 })  
+
+async function fetchWeather(){
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "Kisumu";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    //    
+    }catch(error){
+        console.error('There was an error!',error);
+    }
+}
+fetchWeather();
+
+function displayWeather(){
+    const temperature = document.querySelector('#weather-temp');
+    const weather = document.querySelector('weather');
+    const icon = document.querySelector('weather-icon');
+    const description = document.querySelector('weather-description');
+}
